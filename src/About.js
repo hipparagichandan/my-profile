7d8fd30d08e56453c0b44632c7d28323c132735d
hipparagichandan <113527043+hipparagichandan@users.noreply.github.com>
2022-11-27 23:51:22 +0530

    Update About.js

--- src/About.js
+++ src/About.js
@@ -2,11 +2,7 @@ import React from 'react'
 
 const About = () => {
   return (
-<<<<<<< HEAD
     <div>About me</div>
-=======
-    <div>ABout me -I am changing about line</div>
->>>>>>> 634da66b14d659d6c8b18e6fb25122a8a93892af
   )
 }
 
